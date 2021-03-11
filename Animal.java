e5def1d76a39e4476f69409240ea4f9c331f5248
mirolongo <70956059+mirolongo@users.noreply.github.com>
2021-03-12 00:06:27 +0100

    Initial write of classes
    Created classes:
    Person
    Animal
    Dog

---
+++ Animal.java
@@ -0,0 +1,35 @@
+class Animal{
+    String name;
+    int age;
+    String breed;
+
+    public Animal(String name, int age, String breed) {
+        this.name = name;
+        this.age = age;
+        this.breed = breed;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+}
